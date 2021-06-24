a86f0f4df3422544f1c09a093cf4764190632aee
David Cruwys <david@ideasmen.com.au>
2021-06-24 15:03:29 +1000

    update

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace P02Ef4
 {
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // The quick brown fox
         }
     }
 }
